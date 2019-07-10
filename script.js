9eb1e23e953e47497a2e45175dace1b4dd0f6cf0
Aleksandrovna1 <gritsienko.tatiana@gmail.com>
2019-07-10 12:27:17 +0300

    home work

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// №1 
+    var hours = 3;
+    var minutes = 60;
+    var seconds = 60;
+    var secondsInMinutes = minutes * seconds;
+    var secondsInHours = secondsInMinutes * hours;
+    console.log(secondsInHours);
+// або так №1 
+    var hours = 3;
+    var minutes = 60;
+    var seconds = 60;
+    console.log(hours * minutes * seconds);
+
+// №2
+    var num = 1;
+    num +=12;
+    num -=14;
+    num *=5;
+    num /=7;
+    num +=1;
+    num -=1;
+    alert(num);    
+
+// №3
+ 
+    var a=10;
+    var b=2;
+    var c=5;
+    console.log(a + b + c);
+
+// №4
+ 
+    var a=17;
+    var b=10;
+    var c = a - b;
+    var d=7;
+    var result = c + d;
+    console.log(result);
+
+// №5
+    var message; 
+    message = 'Привiт,';
+    var name;
+    name = 'Тетяна!';
+    console.log(message + ' ' + name);
